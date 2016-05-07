ca029c26effff63d5e4eccc4f316d0cf45eecd76
Vibhanshu Chaturvedi <vibhanshu86@gmail.com>
2016-05-08 01:51:19 +0530

    code cleaning

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,53 @@
 // Initialize a Line chart in the container with the ID chart1
-/* global Chartist, _ */
+/* global c3, _ */
 
 (function () {
   'use strict';
-  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    monthsLabel = _.map(months, function (item) {
-      return item.toUpperCase();
-    }),
-    series = _.times(12, _.random.bind(_, 30, 40));
+  // var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  //   monthsLabel = _.map(months, function (item) {
+  //     return item.toUpperCase();
+  //   }),
+  //   series = _.times(12, _.random.bind(_, 30, 40));
 
-  var chart1  = c3.generate({
+  c3.generate({
+    bindto: '#chart1',
+    axis: {
+      x: {
+        padding: 0
+      },
+      y: {
+        padding: 0,
+        show: false
+      }
+    },
+    legend: {
+      hide: true
+    },
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250, 100, 150, 200]
+      ],
+      type: 'area-spline'
+    },
+    grid: {
+      x: {
+        lines: [
+          {value: 0},
+          {value: 1},
+          {value: 2},
+          {value: 3},
+          {value: 4},
+          {value: 5},
+          {value: 6},
+          {value: 7},
+          {value: 8}
+
+        ]
+      }
+    }
+  });
+
+  c3.generate({
     size: {
       width: 380
     },
@@ -21,7 +59,7 @@
           top: 30
         },
         height: 20
-      },
+      }
     },
     bindto: '#c3-chart-bar-1',
     data: {
@@ -38,24 +76,30 @@
     }
   });
 
-  var chart2 = c3.generate({
+  c3.generate({
     bindto: '#c3-chart-donut-1',
     data: {
       columns: [
         ['data1', 30],
         ['data2', 120]
       ],
-      type : 'donut',
-      onclick: function (d, i) { console.log("onclick", d, i); },
-      onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-      onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+      type: 'donut',
+      onclick: function (d, i) {
+        console.log('onclick', d, i);
+      },
+      onmouseover: function (d, i) {
+        console.log('onmouseover', d, i);
+      },
+      onmouseout: function (d, i) {
+        console.log('onmouseout', d, i);
+      }
     },
     size: {
       width: 150
     },
     donut: {
       width: 6,
-      title: "Iris Petal Width"
+      title: 'Iris Petal Width'
     },
     legend: {
       show: false
@@ -65,24 +109,30 @@
     }
   });
 
-  var chart3 = c3.generate({
+  c3.generate({
     bindto: '#c3-chart-donut-2',
     data: {
       columns: [
         ['data1', 10],
         ['data2', 120]
       ],
-      type : 'donut',
-      onclick: function (d, i) { console.log("onclick", d, i); },
-      onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-      onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+      type: 'donut',
+      onclick: function (d, i) {
+        console.log('onclick', d, i);
+      },
+      onmouseover: function (d, i) {
+        console.log('onmouseover', d, i);
+      },
+      onmouseout: function (d, i) {
+        console.log('onmouseout', d, i);
+      }
     },
     size: {
       width: 150
     },
     donut: {
       width: 6,
-      title: "Iris Petal Width"
+      title: 'Iris Petal Width'
     },
     legend: {
       show: false
@@ -92,7 +142,7 @@
     }
   });
 
-  var chart4  = c3.generate({
+  c3.generate({
     size: {
       width: 380
     },
@@ -104,7 +154,7 @@
           top: 30
         },
         height: 20
-      },
+      }
     },
     bindto: '#c3-chart-bar-2',
     data: {
@@ -121,17 +171,92 @@
     }
   });
 
-  var chart5 = c3.generate({
+  c3.generate({
     bindto: '#c3-chart-5',
+    axis: {
+      x: {
+        type: 'category',
+        categories: ['USA', 'FRA', 'ITA', 'NZ', 'RSA', 'ESP']
+      }
+    },
     data: {
       columns: [
-        ['data1', 30, 200, 100, 400, 150, 250]
+        ['data1', 30, 80, 100, 50, 150, 70]
       ],
       type: 'spline'
+    },
+    grid: {
+      y: {
+        lines: [
+          {value: 0},
+          {value: 20},
+          {value: 40},
+          {value: 60},
+          {value: 80},
+          {value: 100},
+          {value: 120},
+          {value: 140},
+          {value: 160}
+
+        ]
+      }
+    }
+
+  });
+
+  c3.generate({
+    size: {
+      width: 380
+    },
+    axis: {
+      y: {
+        show: false
+      }
+    },
+    bindto: '#c3-chart-bar-3',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250, 30, 100, 400, 150, 250]
+      ],
+      type: 'bar'
+    },
+    legend: {
+      hide: true
+    },
+    bar: {
+      width: 20
     }
   });
 
+  c3.generate({
+    bindto: '#c3-chart-donut-3',
+    data: {
+      columns: [
+        ['data1', 40],
+        ['data2', 120],
+        ['data3', 40],
+        ['data4', 70]
+      ],
+      type: 'donut',
+      onclick: function (d, i) {
+        console.log('onclick', d, i);
+      },
+      onmouseover: function (d, i) {
+        console.log('onmouseover', d, i);
+      },
+      onmouseout: function (d, i) {
+        console.log('onmouseout', d, i);
+      }
+    },
+    size: {
+      width: 360
+    },
+    donut: {
+      width: 6
+    },
+    legend: {
+      position: 'right'
+    }
+  });
 
 }());
-
-
